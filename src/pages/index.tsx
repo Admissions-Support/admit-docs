83096db05bf964333638c6810abeffed8d64e170
Alex <akalapsazov@yahoo.com>
2025-03-17 10:19:33 +0200

    config: remove landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,11 @@
-import type { ReactNode } from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
+import type { ReactNode } from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
+import { Redirect } from "@docusaurus/router";
 
 function Homepage() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+  return <Redirect to="docs/intro" />;
 }
 
 export default function Home(): ReactNode {
